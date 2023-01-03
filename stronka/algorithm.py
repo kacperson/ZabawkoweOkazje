d4d67fd4a721a52720e37c8b35f953e398901855
RlCK <kcpr.hk@gmail.com>
2023-01-03 23:40:48 +0100

    on going work with sorting algorithm

--- stronka/algorithm.py
+++ stronka/algorithm.py
@@ -1,11 +1,20 @@
 import json
+import collections
+import itertools
 
 class SortingAlgorithm:
 
     def __init__(self, products=None, searched=None):
-        self.searched = searched
+        self.isFound = {item : 0 for item in searched}
         f = open("produkty.json")
         self.products = json.load(f)["products"]
+        f.close()
+        self.productsInBlocks = {}
+        self.searchedQTY = len(searched)
+
+    def resetSearched(self):
+        temp = self.isFound.keys()
+        self.isFound = {item : 0 for item in temp}
         self.productsInBlocks = {}
 
     def agregateBy(self, key):
@@ -16,13 +25,62 @@ class SortingAlgorithm:
                 self.productsInBlocks[product[key]] = []
                 self.productsInBlocks[product[key]].append(product)
 
-    
-    
     def show(self):
         print(json.dumps(self.productsInBlocks,indent=2))
+
+    def dataIntoSets(self):
+        ### the fewest shops
+        self.resetSearched()
+        DataTFS = {}
+        self.agregateBy("vendor")
+        
+        tempDict = {tempVendorName: len(self.productsInBlocks[tempVendorName]) for tempVendorName in self.productsInBlocks}
+        tempDict = collections.OrderedDict(sorted(tempDict.items()))
+        tempDict = {tempVendorName: self.productsInBlocks[tempVendorName] for tempVendorName in tempDict}
+
+        self.productsInBlocks = tempDict
+        itemsCounter = 0
+
+        for vendorName, vendorItems in self.productsInBlocks.items():
+            for item in vendorItems:
+                if not self.isFound[item["name"]]:
+                    self.isFound[item["name"]] = 1
+                    itemsCounter += 1
+                    if DataTFS.get(vendorName) != None:
+                        DataTFS[vendorName].append(item)
+                    else:
+                        DataTFS[vendorName] = []
+                        DataTFS[vendorName].append(item)
+            if itemsCounter == self.searchedQTY:
+                break
+        #print(json.dumps(DataTFS,indent=2))
+        
+        ### the lowest price
+        DataTLP = {}
+        self.resetSearched()
+        self.agregateBy("name")
+        tempList = [[item["id"] for item in self.productsInBlocks[name]] for name in self.productsInBlocks]
+        print(tempList)
+        combinationsOfItems = list(itertools.product(*tempList))
+        print(combinationsOfItems)
+        tempDict = {}
+        listOfVendors = []
+        for combination in combinationsOfItems:
+            fullPrice = 0
+            for itemId in combination:
+                fullPrice += self.products[itemId]["price"]
+                if not self.products[itemId]["vendor"] in listOfVendors:
+                    listOfVendors.append(self.products[itemId]["vendor"])
+                    fullPrice += self.products[itemId]["delivery price"]
+            tempDict[combination] = fullPrice
+        tempDict = sorted(tempDict.items(), key=lambda x:x[1])
+        print(tempDict[0])
+        
+
 if __name__ == "__main__":
 
-    sortowanie = SortingAlgorithm()
-    sortowanie.show()
-    sortowanie.agregateBy("vendor")
-    sortowanie.show()
+    searchingFor = ["Klocki Lego 123456", "Motorek", "Book"]
+    sortowanie = SortingAlgorithm(searched=searchingFor)
+    # sortowanie.agregateBy("vendor")
+    # sortowanie.show()
+    sortowanie.dataIntoSets()
