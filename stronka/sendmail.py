9de78ebc9aab678f4d79d3cba94934c131168dce
Dominik Dobrowolski <76428521+ddominet@users.noreply.github.com>
2023-01-08 03:27:13 +0100

    linter
    force black

--- stronka/sendmail.py
+++ stronka/sendmail.py
@@ -1,10 +1,11 @@
 import smtplib
 from email.mime.text import MIMEText
 
+
 def send_verification_email(to, link):
-    sender = 'zabawkowe.okazje@gmail.com'
-    password = 'jgbuxqxdnhivowdw'
-    smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
+    sender = "zabawkowe.okazje@gmail.com"
+    password = "jgbuxqxdnhivowdw"
+    smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
     smtpserver.ehlo()
     smtpserver.starttls()
     smtpserver.ehlo()
@@ -13,12 +14,14 @@ def send_verification_email(to, link):
     message = """\
     Kliknij w poniższy link aby aktywować konto Zabawkowe Okazje:
     <a href="{}">{}</a>
-    """.format(link, link)
-    msg = MIMEText(message, 'html')
-    msg['Subject'] = 'Weryfikacja e-mail Zabawkowe Okazje'
-    msg['From'] = sender
-    msg['To'] = to
+    """.format(
+        link, link
+    )
+    msg = MIMEText(message, "html")
+    msg["Subject"] = "Weryfikacja e-mail Zabawkowe Okazje"
+    msg["From"] = sender
+    msg["To"] = to
 
     smtpserver.sendmail(sender, to, msg.as_string())
-    print('Verification email sent!')
+    print("Verification email sent!")
     smtpserver.close()
