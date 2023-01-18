a387ed8bc7c812f0f5eba9d7cf443fd9ba6833fa
Dominik Dobrowolski <76428521+ddominet@users.noreply.github.com>
2023-01-18 02:04:56 +0100

    Update __init__.py

--- stronka/__init__.py
+++ stronka/__init__.py
@@ -4,14 +4,17 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from stronka.ceneo import my_ceneo
 
+
 app = Flask(__name__)
 app.debug = True
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://postgres:test@db/zabawkowe"
 app.config['SECRET_KEY'] = 'ACompl1cat3dText.'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///WWWW.db'
-app.config['SQLITE_DATABASE_CHARSET'] = 'cp1252'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
 from stronka import routes
+db.create_all()
