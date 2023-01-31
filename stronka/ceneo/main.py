b9f02772d65fe8e28b03030806841c0506a559e9
RlCK <kcpr.hk@gmail.com>
2023-01-31 18:28:29 +0100

    refactoring

--- stronka/ceneo/main.py
+++ stronka/ceneo/main.py
@@ -4,8 +4,8 @@ from stronka.ceneo.my_ceneo import Ceneo
 
 
 def ceneo_scrapper(list):
-    listy_propozycji = {}
-    produkty = []   
+    propositionsDict = {}
+    products = []   
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
     options = Options()
     options.add_argument("--headless")
@@ -20,34 +20,34 @@ def ceneo_scrapper(list):
     
     driver.maximize_window()
     bot = Ceneo(driver)
-    pierwszy = True
-    for elem in list:
-        if not pierwszy:
+    isFirst = True
+    for element in list:
+        if not isFirst:
             driver.execute_script("window.open('');")
             driver.switch_to.window(
                 driver.window_handles[len(driver.window_handles) - 1]
             )
             driver.get(
-                f"https://www.ceneo.pl/Zabawki;szukaj-{elem.replace(' ', '+')};0112-0.htm"
+                f"https://www.ceneo.pl/Zabawki;szukaj-{element.replace(' ', '+')};0112-0.htm"
             )
         else:
             driver.get(
-                f"https://www.ceneo.pl/Zabawki;szukaj-{elem.replace(' ', '+')};0112-0.htm"
+                f"https://www.ceneo.pl/Zabawki;szukaj-{element.replace(' ', '+')};0112-0.htm"
             )
-            pierwszy = False
+            isFirst = False
     for i in range(0, len(list)):
-        xd = []
+        tempList = []
         driver.switch_to.window(driver.window_handles[i])
-        xd, lista_propozycji = bot.zwrotlista()
-        if xd != []:
-            lista_numer = []
-            for j in range(0, len(xd)):
-                lista_numer.append(f"num{str(j)}")
-            zipped = dict(zip(lista_numer, xd))
-            listy_propozycji[f"{list[i]}"] = zipped
+        tempList, propositionsList = bot.zwrotlista()
+        if tempList != []:
+            numbersList = []
+            for j in range(0, len(tempList)):
+                numbersList.append(f"num{str(j)}")
+            zipped = dict(zip(numbersList, tempList))
+            propositionsDict[f"{list[i]}"] = zipped
         else:
-            tab = bot.wyszukiwanie(lista_propozycji, 1)
-            for i, slownik in enumerate(tab):
-                slownik["ID"] = len(produkty) + i
-            produkty.extend(tab)
-    return produkty, listy_propozycji
+            resultsTable = bot.wyszukiwanie(propositionsList, 1)
+            for i, tempDict in enumerate(resultsTable):
+                tempDict["ID"] = len(products) + i
+            products.extend(resultsTable)
+    return products, propositionsDict
