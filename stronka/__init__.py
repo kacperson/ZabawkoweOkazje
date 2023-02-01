6ed0fd5be847bf155e1db8e9d43fd78b17748553
Dominik Dobrowolski <76428521+ddominet@users.noreply.github.com>
2023-02-01 20:38:21 +0100

    Update __init__.py

--- stronka/__init__.py
+++ stronka/__init__.py
@@ -17,4 +17,5 @@ login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
 from stronka import routes
-db.create_all()
+with app.app_context():
+    db.create_all()
